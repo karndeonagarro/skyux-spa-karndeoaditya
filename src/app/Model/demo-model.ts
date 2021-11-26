b087fd1a7474a1cf39c360180a523ea7e2739f0d
Karndeo Aditya <karndeo.aditya@nagarro.com>
2021-11-26 15:30:01 +0530

    remove all code

--- src/app/Model/demo-model.ts
+++
@@ -1,8 +0,0 @@
-export class DemoModel {
-  public firstName: String;
-  public lastName: String;
-  public contactNo: String;
-  public email: String;
-  public dob: String;
-  public address: String;
-}
